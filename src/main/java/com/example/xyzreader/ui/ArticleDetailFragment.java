ce4ef0d51fc6de8a6bc5197c0e2e0d7b4ec95f46
Brett Tucker <bretttucker@gmail.com>
2016-01-30 15:49:39 -0700

    Remove unused files

--- src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
+++ src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
@@ -13,8 +13,6 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
-import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.example.xyzreader.R;
