8ae87ba4434c0438d9a7a3c9c44b342240405576
Brett Tucker <bretttucker@gmail.com>
2016-01-30 15:43:22 -0700

    Add image over status bar
    Adds a style for version 21 that will allow the image to take over the
    status bar.

--- src/main/java/com/example/xyzreader/ui/ArticleDetailActivity.java
+++ src/main/java/com/example/xyzreader/ui/ArticleDetailActivity.java
@@ -7,17 +7,14 @@ import android.content.Loader;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.drawable.ColorDrawable;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v13.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowInsets;
 import android.widget.ImageView;
 
 import com.android.volley.VolleyError;
@@ -29,9 +26,7 @@ import com.example.xyzreader.data.ItemsContract;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-/**
- * An activity representing a single Article detail screen, letting you swipe between articles.
- */
+
 public class ArticleDetailActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
     private Cursor mCursor;
@@ -55,11 +50,7 @@ public class ArticleDetailActivity extends AppCompatActivity implements LoaderMa
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            getWindow().getDecorView().setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
-                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
-        }
+
         setContentView(R.layout.activity_article_detail);
 
         ButterKnife.bind(this);
@@ -101,8 +92,6 @@ public class ArticleDetailActivity extends AppCompatActivity implements LoaderMa
                 onSupportNavigateUp();
             }
         });
-
-
 
         if (savedInstanceState == null) {
             if (getIntent() != null && getIntent().getData() != null) {
